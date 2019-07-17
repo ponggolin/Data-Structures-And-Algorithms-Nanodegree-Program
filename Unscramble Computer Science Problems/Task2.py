ffdc9045efb5d01b671786a378d475ffa92767e1
Jason Lin <ponggolin@gmail.com>
2019-07-17 18:49:59 +0800

    Update Task2.py

--- Unscramble Computer Science Problems/Task2.py
+++ Unscramble Computer Science Problems/Task2.py
@@ -19,4 +19,19 @@ Print a message:
 "<telephone number> spent the longest time, <total time> seconds, on the phone during 
 September 2016.".
 """
+longest_num = None
+longest_duration = 0
+tel_num_duration = {}
 
+for record in calls:
+    for i in range(2):
+        tel_num = record[i]
+        duration = int(record[-1])
+        
+        tel_num_duration[tel_num] =  tel_num_duration.get(tel_num, 0) + duration
+        
+        if tel_num_duration[tel_num] > longest_duration:
+            longest_duration = tel_num_duration[tel_num]
+            longest_num = tel_num
+
+print("{} spent the longest time, {} seconds, on the phone during September 2016.".format(longest_num, longest_duration))
